f13e0e84d35c76e0e84e07cc6632ae4a9f89d747
Gurat <g.jatianom@students.ac.id>
2026-04-09 10:08:08 +0700

    feat: melengkapi fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,20 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		colsole.WriteLine("hasil penambahan: {0} + {1} = {2]", a, b, Penambahan(a, b));
+		colsole.WriteLine("hasil Pengurangan: {0} + {1} = {2}", a, b, Pengurangan(a, b));
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
